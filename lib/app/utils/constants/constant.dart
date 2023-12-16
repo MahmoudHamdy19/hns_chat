dc207fd6f7c63af4cee760ffb614b1957b4409f1
mahmoudmavia19 <hamdymhmoud31@gamil.com>
2023-12-17 00:02:28 +0200

    handle update constant.dart

--- lib/app/utils/constants/constant.dart
+++ lib/app/utils/constants/constant.dart
@@ -1,3 +1,39 @@
 class FireStoreEndPoints {
   static const String users = 'users';
 }
+
+String? validateWithRegex(
+    String? value, String regexPattern, String errorMessage) {
+  if (value == null || value.isEmpty) {
+    return 'Validation error: Value cannot be empty';
+  }
+
+  RegExp regex = RegExp(regexPattern);
+  if (!regex.hasMatch(value)) {
+    return errorMessage;
+  }
+
+  return null;
+}
+
+String? validateUsername(String? value) {
+  return validateWithRegex(
+      value, r'^[a-zA-Z0-9_]+$', 'Invalid username format');
+}
+
+String? validateEmail(String? value) {
+  return validateWithRegex(
+      value,
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+      'Enter a valid email address');
+}
+
+String? validatePhoneNumber(String? value) {
+  return validateWithRegex(value, r'^\+20[0-9]{9}$',
+      'Enter a valid phone number starting with +20 and followed by 9 digits');
+}
+
+String? validatePassword(String? value) {
+  return validateWithRegex(
+      value, r'^(.{6,})$', 'Password must be at least 6 characters');
+}
--- lib/ui/auth/register/regiser_screen.dart
+++ lib/ui/auth/register/regiser_screen.dart
@@ -5,9 +5,10 @@ import 'package:hns_chat/app/widgets/costum_text_form.dart';
 import 'package:hns_chat/app/widgets/custom_text_password.dart';
 import 'package:hns_chat/ui/auth/register/controller/register_controller.dart';
 
+import '../../../app/utils/constants/constant.dart';
+
 class RegisterScreen extends GetWidget<RegisterController> {
   final GlobalKey<FormState> _key = GlobalKey();
-
   @override
   Widget build(BuildContext context) {
     return Obx(() {
@@ -25,7 +26,6 @@ class RegisterScreen extends GetWidget<RegisterController> {
               : _scaffoldBody());
     });
   }
-
   _scaffoldBody() => Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(20.0),
@@ -34,10 +34,6 @@ class RegisterScreen extends GetWidget<RegisterController> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /*   CustomTextForm(title: 'Name',
-               onChanged: (value) {
-                controller.user!.userName
-               },),*/
                 CustomTextForm(
                   title: 'User Name',
                   onChanged: (value) {
@@ -107,38 +103,3 @@ class RegisterScreen extends GetWidget<RegisterController> {
       );
 }
 
-String? validateWithRegex(
-    String? value, String regexPattern, String errorMessage) {
-  if (value == null || value.isEmpty) {
-    return 'Validation error: Value cannot be empty';
-  }
-
-  RegExp regex = RegExp(regexPattern);
-  if (!regex.hasMatch(value)) {
-    return errorMessage;
-  }
-
-  return null;
-}
-
-String? validateUsername(String? value) {
-  return validateWithRegex(
-      value, r'^[a-zA-Z0-9_]+$', 'Invalid username format');
-}
-
-String? validateEmail(String? value) {
-  return validateWithRegex(
-      value,
-      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-      'Enter a valid email address');
-}
-
-String? validatePhoneNumber(String? value) {
-  return validateWithRegex(value, r'^\+20[0-9]{9}$',
-      'Enter a valid phone number starting with +20 and followed by 9 digits');
-}
-
-String? validatePassword(String? value) {
-  return validateWithRegex(
-      value, r'^(.{6,})$', 'Password must be at least 6 characters');
-}
